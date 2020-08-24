9be5d9f9f8b29967c7fbfb432e5ab24bae92e02b
liaojikunwork <liaojikundever@gmail.com>
2020-08-24 04:55:38 -0500

    update switch animation (#24)
    * update switch animation
    
    * "clicking on logo" switch

--- src/client/pages/Home/index.tsx
+++ src/client/pages/Home/index.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect } from 'react';
-import { useObserver } from 'mobx-react-lite';
-import './index.scss';
-import { ClientOnly } from '../../components';
-import { ERCXRC, XRCERC, SwitchHeader, CompleteFrame } from './components';
-import { useStore } from '../../../common/store';
-import { CARD_XRC20_ERC20, CARD_ERC20_XRC20 } from '../../../common/store/base';
-import { matchPath, useHistory } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useObserver } from "mobx-react-lite";
+import "./index.scss";
+import { ClientOnly } from "../../components";
+import { ERCXRC, XRCERC, SwitchHeader, CompleteFrame } from "./components";
+import { useStore } from "../../../common/store";
+import { CARD_XRC20_ERC20, CARD_ERC20_XRC20 } from "../../../common/store/base";
+import { matchPath, useHistory } from "react-router-dom";
 
 export const Home = () => {
   const { base } = useStore();
   const history = useHistory();
 
   const isERCXRC = !!matchPath(history.location.pathname, {
-    path: '/eth',
+    path: "/eth",
     exact: true,
   });
 
@@ -20,8 +20,8 @@ export const Home = () => {
     base.setMode(isERCXRC ? CARD_ERC20_XRC20 : CARD_XRC20_ERC20);
   }, [isERCXRC]);
 
-  const switchTo = (path) => () => {
-    history.push(path);
+  const switchTo = () => {
+    history.push(base.mode === CARD_ERC20_XRC20 ? "/iotx" : "/eth");
   };
 
   return useObserver(() => (
@@ -33,34 +33,29 @@ export const Home = () => {
               <CompleteFrame isERCXRC={isERCXRC} />
             </div>
           ) : (
-            <>
-              <div
-                className={`page__home__content__frame bg-primary rounded-md ${
-                  isERCXRC
-                    ? 'page__home__content__frame--active'
-                    : 'page__home__content__frame--inactive'
-                }`}
-              >
-                <SwitchHeader
-                  onSwitch={switchTo('/iotx')}
-                  mode={CARD_ERC20_XRC20}
-                />
-                <ERCXRC />
-              </div>
-              <div
-                className={`page__home__content__frame bg-secondary rounded-md ${
-                  isERCXRC
-                    ? 'page__home__content__frame--inactive'
-                    : 'page__home__content__frame--active'
-                }`}
-              >
-                <SwitchHeader
-                  onSwitch={switchTo('/eth')}
-                  mode={CARD_XRC20_ERC20}
-                />
-                <XRCERC />
-              </div>
-            </>
+            <div className="rounded-t-md overflow-hidden">
+              <SwitchHeader onSwitch={switchTo} isERCXRC={isERCXRC} />
+              <>
+                <div
+                  className={`page__home__content__frame bg-primary rounded-b-md overflow-hidden ${
+                    isERCXRC
+                      ? "page__home__content__frame--active"
+                      : "page__home__content__frame--inactive"
+                  }`}
+                >
+                  <ERCXRC />
+                </div>
+                <div
+                  className={`page__home__content__frame bg-secondary rounded-b-md overflow-hidden ${
+                    isERCXRC
+                      ? "page__home__content__frame--inactive"
+                      : "page__home__content__frame--active"
+                  }`}
+                >
+                  <XRCERC />
+                </div>
+              </>
+            </div>
           )}
         </div>
       </div>

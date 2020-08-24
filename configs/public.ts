74bec2972cb0ad76a89d0d46a5afd57d98cef6ee
uuz <ncysatnaf@gmail.com>
2020-08-24 15:15:36 +0800

    add sentry

--- configs/public.ts
+++ configs/public.ts
@@ -9,10 +9,12 @@ const {
   APP_PORTIS_ID,
   CASHIER_CONTRACT_ADDRESS_1, // MAINNET
   CASHIER_CONTRACT_ADDRESS_3, // ROSPTEN
+  SENTRY_DSN,
 } = utils.env.getEnv();
 
 export const publicConfig = {
   IS_PROD,
+  NODE_ENV,
   IOTEX_CORE_ENDPOPINT: IS_PROD
     ? "https://api.iotex.one:443"
     : "https://api.testnet.iotex.one:443",
@@ -21,4 +23,7 @@ export const publicConfig = {
   APP_PORTIS_ID,
   CASHIER_CONTRACT_ADDRESS_1,
   CASHIER_CONTRACT_ADDRESS_3,
+  SENTRY_DSN:
+    SENTRY_DSN ||
+    "https://89e3100998d54964b65a16ada1edeabc@sentry.gcap1.iotex.io/3",
 };
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import 'mobx-react/batchingForReactDom';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from "@sentry/react";
+import { utils } from "../common/utils/index";
+import { publicConfig } from "../../configs/public";
 
 import 'antd/dist/antd.css';
 
 import App from './App';
+
+utils.env.onBrowser(() => {
+  Sentry.init({
+    dsn: publicConfig.SENTRY_DSN,
+    environment: `client-${publicConfig.NODE_ENV}`,
+  });
+});
 
 const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate;
 

652c5fb785b37fbb925b559feed8cf0e5080f0e9
Jikun Liao <liaojikundever@gmail.com>
2020-08-25 07:49:12 -0400

    please follow design on confirm dialog #37

--- src/client/components/ConfirmModal/index.tsx
+++ src/client/components/ConfirmModal/index.tsx
@@ -28,36 +28,30 @@ export const ConfirmModal = (props: IComponentProps) => {
       visible={props.visible}
       onCancel={props.close}
       footer={null}
-      className="modal__confirm_deposit"
+      className={`modal__confirm_deposit ${
+        props.isERCXRC ? "modal__confirm_deposit--ercxrc" : "modal__confirm_deposit--xrcerc"
+      }`}
     >
-      <div className="c-gray flex items-center">
+      <div className="c-white flex items-center">
         <span className="font-normal text-3xl mr-3">{props.depositAmount}</span>
         {props.depositToken && (
           <>
             <CurrencyLogo currency={props.depositToken} />
-            <span className="text-xl ml-2 font-light">
-              {props.depositToken.name}
-            </span>
+            <span className="text-xl ml-2 font-light">{props.depositToken.name}</span>
           </>
         )}
       </div>
-      <div className="c-gray font-thin text-base mt-2 mb-5">
-        {props.middleComment}
-      </div>
-      <div className="c-gray flex items-center">
+      <div className="c-gray font-thin text-base mt-2 mb-5">{props.middleComment}</div>
+      <div className="c-white flex items-center">
         <span className="font-normal text-3xl mr-3">{props.depositAmount}</span>
         {props.depositToken && (
           <>
             <CurrencyLogo currency={props.depositToken} />
-            <span className="text-xl ml-2 font-light">
-              {props.depositToken.symbol}
-            </span>
+            <span className="text-xl ml-2 font-light">{props.depositToken.symbol}</span>
           </>
         )}
       </div>
-      <div className="c-gray font-thin text-base mt-2 mb-5">
-        on {props.mintToken ? props.mintToken.name : ""}
-      </div>
+      <div className="c-gray font-thin text-base mt-2 mb-5">on {props.mintToken ? props.mintToken.name : ""}</div>
       <div className="my-6 text-left c-gray">
         <div className="font-normal text-base mb-3">{lang.t("fee")}</div>
         <div className="font-light text-sm flex items-center justify-between">
@@ -65,9 +59,7 @@ export const ConfirmModal = (props: IComponentProps) => {
           <span>0 ({lang.t("free")})</span>
         </div>
         <div className="font-light text-sm flex items-center justify-between">
-          <span>
-            {lang.t(props.isERCXRC ? "relay_to_iotex" : "relay_to_ethereum")}
-          </span>
+          <span>{lang.t(props.isERCXRC ? "relay_to_iotex" : "relay_to_ethereum")}</span>
           <span>0 ({lang.t("free")})</span>
         </div>
       </div>
@@ -75,9 +67,7 @@ export const ConfirmModal = (props: IComponentProps) => {
         <Button
           onClick={props.onConfirm}
           className={`modal__confirm_deposit__confirm ${
-            props.isERCXRC
-              ? "modal__confirm_deposit__confirm--erc-xrc"
-              : "modal__confirm_deposit__confirm--xrc-erc"
+            props.isERCXRC ? "modal__confirm_deposit__confirm--erc-xrc" : "modal__confirm_deposit__confirm--xrc-erc"
           } w-full c-white`}
           type="primary"
         >

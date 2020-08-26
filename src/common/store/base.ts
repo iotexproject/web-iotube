f992ca12ddb3f0f4e892b2c9b2ae39ccf43b635e
liaojikunwork <liaojikundever@gmail.com>
2020-08-26 05:27:47 -0500

    confirmation dialog eth->iotx #46 (#49)
    * confirmation dialog eth->iotx #46
    
    * update confirm modal

--- src/common/store/base.ts
+++ src/common/store/base.ts
@@ -1,16 +1,28 @@
-import { observable, action, computed } from 'mobx';
-import remotedev from 'mobx-remotedev';
+import { observable, action, computed } from "mobx";
+import remotedev from "mobx-remotedev";
 
-export const CARD_ERC20_XRC20 = 'ERC20-XRC20';
-export const CARD_XRC20_ERC20 = 'XRC20-ERC20';
-@remotedev({ name: 'base' })
+export const CARD_ERC20_XRC20 = "ERC20-XRC20";
+export const CARD_XRC20_ERC20 = "XRC20-ERC20";
+@remotedev({ name: "base" })
 export class BaseStore {
-  @observable NODE_ENV = '';
+  @observable NODE_ENV = "";
   @observable mode = CARD_ERC20_XRC20;
   @observable showComplete = false;
+
+  @observable hash = "";
+  @observable link = "";
+  @observable address = "";
 
   @action.bound
   setMode(mode) {
     this.mode = mode;
+  }
+
+  @action.bound
+  toggleComplete(hash = "", link = "", address = "") {
+    this.showComplete = !this.showComplete;
+    this.hash = hash;
+    this.link = link;
+    this.address = address;
   }
 }

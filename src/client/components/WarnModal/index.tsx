e4cf502feadfa2047fdcc69a0932d089e3c72ebc
Hung Le <hunglmtb@gmail.com>
2020-09-01 18:40:05 +0700

    fix modal

--- src/client/components/WarnModal/index.tsx
+++ src/client/components/WarnModal/index.tsx
@@ -1,8 +1,6 @@
 import React, { MouseEventHandler } from "react";
 import { Button, Modal } from "antd";
 import { useStore } from "../../../common/store";
-import { Token } from "@uniswap/sdk";
-import CurrencyLogo from "../CurrencyLogo/index";
 
 interface IComponentProps {
   visible: boolean;
@@ -15,28 +13,16 @@ export const WarnModal = (props: IComponentProps) => {
   if (!props.visible) return null;
 
   return (
-    <Modal
-      title={lang.t("warning")}
-      visible={props.visible}
-      onCancel={props.close}
-      footer={null}
-      className={`modal__warn ${
-        props.isERCXRC ? "modal__warn--ercxrc" : "modal__warn--xrcerc"
-      }`}
-    >
+    <Modal title={lang.t("warning")} visible={props.visible} onCancel={props.close} footer={null} className={`modal__warn ${props.isERCXRC ? "modal__warn--ercxrc" : "modal__warn--xrcerc"}`}>
       <div className="text-sm c-white font-light mb-6">
         {props.isERCXRC ? (
           <div className="text-base mb-2">{lang.t("warning.eth.comment")}</div>
         ) : (
           <>
-            <div className="text-base mb-2">
-              {lang.t("warning.iotx.comment.mobile")}
-            </div>
+            <div className="text-base mb-2">{lang.t("warning.iotx.comment.mobile")}</div>
             <div className="text-base mb-2">
               {lang.t("warning.iotx.comment.desktop1")}&nbsp;
-              <a href="https://iopay.iotex.io/desktop">
-                {lang.t("warning.iotx.comment.desktop2")}
-              </a>
+              <a href="https://iopay.iotex.io/desktop">{lang.t("warning.iotx.comment.desktop2")}</a>
               &nbsp;
               {lang.t("warning.iotx.comment.desktop3")}
             </div>
@@ -44,11 +30,7 @@ export const WarnModal = (props: IComponentProps) => {
         )}
       </div>
       <div>
-        <Button
-          onClick={props.close}
-          className={`modal__warn__confirm w-full c-white`}
-          type="primary"
-        >
+        <Button onClick={props.close} className={`modal__warn__confirm w-full c-white`} type="primary">
           {lang.t("ok")}
         </Button>
       </div>

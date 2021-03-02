28c1c7d9afe795f8d9a7233ee1b43dcbca6479cb
anshuomomo <shuo@iotex.io>
2021-03-02 10:03:11 +0800

    ui update

--- src/client/pages/Home/components/SwitchHeader/index.tsx
+++ src/client/pages/Home/components/SwitchHeader/index.tsx
@@ -3,6 +3,7 @@ import "./index.scss";
 import { useStore } from "../../../../../common/store";
 import { useObserver, useLocalStore } from "mobx-react";
 import { useSwipeable } from "react-swipeable";
+import { DownOutlined } from "@ant-design/icons/lib";
 
 const IMG_ETH = require("../../../../static/images/logo-ethereum.png");
 const IMG_IOTEX = require("../../../../static/images/logo-iotex.png");
@@ -48,57 +49,56 @@ export const SwitchHeader = (props: IComponentProps) => {
   });
 
   return useObserver(() => (
-    <div
-      className={`page__home__component__switch_header ${
-        store.isERCXRC ? "bg-secondary" : "bg-primary"
-      }`}
-    >
-      <div
-        {...ethHandlers}
-        className={`page__home__component__switch_header__item page__home__component__switch_header__item--ethereum ${
-          store.isERCXRC
-            ? "page__home__component__switch_header__item--active"
-            : "page__home__component__switch_header__item--inactive"
-        } flex-1 flex flex-col justify-center items-center bg-primary c-white py-8 `}
-      >
-        <div className="flex items-center select-none flex-col">
-          <img
-            src={IMG_ETH}
-            className={`h-20 ${!store.isERCXRC ? "cursor-pointer" : ""}`}
-            onClick={() => {
-              if (!store.isERCXRC) store.toggleERCXRC();
-            }}
-          />
-          <div className="text-xl font-light -mt-2">
-            {lang.t("token.ethereum")}
+    <div style={{ position: "relative" }}>
+      <div className={`page__home__component__switch_header ${store.isERCXRC ? "bg-secondary" : "bg-primary"}`}>
+        <div
+          {...ethHandlers}
+          className={`page__home__component__switch_header__item page__home__component__switch_header__item--ethereum ${
+            store.isERCXRC ? "page__home__component__switch_header__item--active" : "page__home__component__switch_header__item--inactive"
+          } flex-1 flex flex-col justify-center items-center bg-primary c-white py-8 `}
+        >
+          <div className="flex items-center select-none flex-col">
+            <img
+              src={IMG_ETH}
+              className={`h-20 ${!store.isERCXRC ? "cursor-pointer" : ""}`}
+              onClick={() => {
+                if (!store.isERCXRC) store.toggleERCXRC();
+              }}
+            />
+            <div className="text-xl font-light -mt-2 flex items-center select-none flex-column cursor-pointer">
+              <div className="w-10" />
+              <div>{lang.t("token.ethereum")}</div>
+              <div>
+                <DownOutlined className="ml-4 float-right c-gray" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <img src={IMG_SWITCH} className="page__home__component__switch_header__switch cursor-pointer" onClick={store.toggleERCXRC} />
+        <div
+          {...iotxHandlers}
+          className={`page__home__component__switch_header__item page__home__component__switch_header__item--iotex ${
+            !store.isERCXRC ? "page__home__component__switch_header__item--active" : "page__home__component__switch_header__item--inactive"
+          } flex-1 flex flex-col justify-center items-center bg-secondary c-white py-8`}
+        >
+          <div className="flex items-center flex-col select-none">
+            <img
+              src={IMG_IOTEX}
+              className={`h-20 ${store.isERCXRC ? "cursor-pointer" : ""}`}
+              onClick={() => {
+                if (store.isERCXRC) store.toggleERCXRC();
+              }}
+            />
+            <div className="text-xl font-light -mt-2">{lang.t("token.iotex")}</div>
           </div>
         </div>
       </div>
-      <img
-        src={IMG_SWITCH}
-        className="page__home__component__switch_header__switch cursor-pointer"
-        onClick={store.toggleERCXRC}
-      />
-      <div
-        {...iotxHandlers}
-        className={`page__home__component__switch_header__item page__home__component__switch_header__item--iotex ${
-          !store.isERCXRC
-            ? "page__home__component__switch_header__item--active"
-            : "page__home__component__switch_header__item--inactive"
-        } flex-1 flex flex-col justify-center items-center bg-secondary c-white py-8`}
-      >
-        <div className="flex items-center flex-col select-none">
-          <img
-            src={IMG_IOTEX}
-            className={`h-20 ${store.isERCXRC ? "cursor-pointer" : ""}`}
-            onClick={() => {
-              if (store.isERCXRC) store.toggleERCXRC();
-            }}
-          />
-          <div className="text-xl font-light -mt-2">
-            {lang.t("token.iotex")}
-          </div>
-        </div>
+      <div style={{ width: 225, position: "absolute" }}>
+        <ul>
+          <li>Eth</li>
+          <li>Heco</li>
+          <li>BSC</li>
+        </ul>
       </div>
     </div>
   ));
